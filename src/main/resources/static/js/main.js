f5a68392317152b46b8528f3fc52484124866d22
Seagull <cn_stdio@163.com>
2018-12-15 17:07:39 +0800

    更新了鼠标动画与样式

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -21,3 +21,54 @@ function handleMouseMove(event) {
 }
 
 window.addEventListener('mousemove', handleMouseMove, false);
+
+/* 名片随页面滚动 */
+window.onload= function() {
+    var oDiv = document.getElementById("main-right-div"),
+        H = 0,
+        Y = oDiv;
+    while (Y) {
+        H += Y.offsetTop;
+        Y = Y.offsetParent;
+    }
+    window.onscroll = function()
+    {
+        var s = document.body.scrollTop || document.documentElement.scrollTop;
+        if(s>H-50) {
+            oDiv.style = "position:fixed;top:50px;";
+        } else {
+            oDiv.style = "";
+        }
+    }
+};
+
+/* 鼠标点击出现颜文字 */
+ var a_idx = 0;
+ jQuery(document).ready(function ($) {
+     $("body").click(function (e) {
+         var a = new Array("(๑•̀ㅂ•́)و✧", "ヽ(✿ﾟ▽ﾟ)ノ", "w(ﾟДﾟ)w", "Σ(°△°|||)︴", "(u‿ฺu✿ฺ)", "φ(≧ω≦*)♪","o(*≧▽≦)ツ┏━┓","o(￣ヘ￣o＃)","~(﹁ ﹁)~~","┑(￣Д ￣)┍","(。﹏。*)");
+         var $i = $("<span></span>").text(a[a_idx]);
+         a_idx = (a_idx + 1) % a.length;
+         var x = e.pageX,
+             y = e.pageY;
+         $i.css({
+             "z-index": 99,
+             "top": y - 20,
+             "left": x,
+             "position": "absolute",
+             "font-weight": "bold",
+             "color": "rgb(" + ~~(255 * Math.random()) + "," + ~~(
+                 255 * Math.random()) + "," + ~~(255 * Math.random()) +
+                 ")"
+         });
+         $("body").append($i);
+         $i.animate({
+                 "top": y - 180,
+                 "opacity": 0
+             },
+             1500,
+             function () {
+                 $i.remove();
+             });
+     });
+ });
